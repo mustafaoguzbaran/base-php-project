f414f01cab0b218c177510177fd73fe3559ddc00
mustafaoguzbaran <mustafaoguzbaran@icloud.com>
2023-03-13 17:26:29 +0300

    CRUD Operations

---
+++ config/post_insert.php
@@ -0,0 +1,28 @@
+<?php
+require_once "../database/conn.php";
+$dirname = dirname($_SERVER['SCRIPT_NAME']);
+$dizin = str_replace("/config/post_insert.php", null, $_SERVER['REQUEST_URI']);
+$dosyaYolu = "http://".$_SERVER['HTTP_HOST'].$dizin . "/upload";
+$dosyaAdiAl = $_FILES["post_img"]["name"];
+$_POST['post_img'] = $dosyaYolu . $dosyaAdiAl;
+move_uploaded_file($dosyaAdiAl, $dosyaYolu);
+echo $dosyaYolu;
+if(isset($_POST['insert-post'])) {
+$post = $conn->prepare("INSERT INTO posts set
+post_title = :post_title,
+post_desc = :post_desc,
+post_img = :post_img,
+post_content = :post_content
+");
+$insertPost = $post ->execute(array(
+'post_title' => $_POST['post_title'],
+'post_desc' => $_POST['post_desc'],
+'post_content' => $_POST['post_content'],
+'post_img' => $_POST['post_img']
+));
+}
+if($insertPost){
+Header("Location: http://localhost:8888/PHP-myBLOG/");
+}
+
+?>
